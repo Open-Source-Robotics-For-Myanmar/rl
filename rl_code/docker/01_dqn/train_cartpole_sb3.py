3b830b85e726add3951b93e36d4487efd70dbae9
Pyae Sone Aung <server01.psa1981@gmail.com>
2026-09-03 11:07:53 +0630

    update

--- rl_code/docker/01_dqn/train_cartpole_sb3.py
+++ rl_code/docker/01_dqn/train_cartpole_sb3.py
@@ -3,10 +3,13 @@ from stable_baselines3 import DQN
 
 # CartPole environment ကို ဖန်တီးသည်။
 env = gym.make("CartPole-v1")
+
 # MLP policy သုံးသော DQN model ကို TensorBoard log path နှင့် ဖန်တီးသည်။
 model = DQN("MlpPolicy", env, verbose=1, tensorboard_log="./dqn_cartpole_tensorboard/")
+
 # Environment နှင့် action 100,000 ကြိမ် အပြန်အလှန်လုပ်၍ model ကို train လုပ်သည်။
 model.learn(total_timesteps=100000)
+
 # Train ပြီးသော model ကို နောက်မှ test လုပ်ရန် သိမ်းဆည်းသည်။
 model.save("rom_dqn_cartpole")
 
