034ecac33daf05e9ed837159ad590013ecbe8200
Pyae Soan Aung <server01.psa1981@gmail.com>
2026-09-08 15:23:27 +0630

    SAC

--- rl_code/docker/11_sac/test_cleanrl_halfcheetah.py
+++ rl_code/docker/11_sac/test_cleanrl_halfcheetah.py
@@ -7,6 +7,7 @@ import torch
 from train_halfcheetah_cleanrl_sac import Actor, ENV_ID, MODEL_PATH
 
 ROM_TIME_CHECK = True
+SIMULATION_DELAY = 1 / 30
 env = gym.make(ENV_ID, render_mode="human")
 network_env = gym.vector.SyncVectorEnv([lambda: gym.make(ENV_ID)])
 actor = Actor(network_env)
@@ -24,6 +25,7 @@ while not done:
 	with torch.no_grad():
 		_, _, action = actor.get_action(torch.as_tensor(observation, dtype=torch.float32).unsqueeze(0))
 	observation, reward, terminated, truncated, _ = env.step(action.squeeze(0).numpy())
+	time.sleep(SIMULATION_DELAY)
 	step_count += 1
 	total_reward += reward
 	done = terminated or truncated
--- rl_code/docker/11_sac/test_sb3_halfcheetah.py
+++ rl_code/docker/11_sac/test_sb3_halfcheetah.py
@@ -7,6 +7,7 @@ from stable_baselines3 import SAC
 ENV_ID = "HalfCheetahBulletEnv-v0"
 MODEL_PATH = "rom_sb3_sac_halfcheetah"
 ROM_TIME_CHECK = True
+ROM_PLAYBACK_FPS = 30
 
 env = gym.make(ENV_ID, render_mode="human")
 model = SAC.load(MODEL_PATH)
@@ -16,11 +17,15 @@ done = False
 step_count = 0
 total_reward = 0.0
 while not done:
+	step_started_at = time.perf_counter()
 	action, _states = model.predict(observation, deterministic=True)
 	observation, reward, terminated, truncated, _ = env.step(action)
 	step_count += 1
 	total_reward += reward
 	done = terminated or truncated
+	remaining_frame_time = (1 / ROM_PLAYBACK_FPS) - (time.perf_counter() - step_started_at)
+	if remaining_frame_time > 0:
+		time.sleep(remaining_frame_time)
 
 print(f"Testing complete. Steps: {step_count}, Reward: {total_reward:.1f}")
 if ROM_TIME_CHECK:
